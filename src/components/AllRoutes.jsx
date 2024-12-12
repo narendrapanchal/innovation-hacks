fae249b199788a6acde63d4c017b4d0c697e762c
narendrapanchal <narendrapanchal020@gmail.com>
2024-12-12 11:03:54 +0530

    init

---
+++ src/components/AllRoutes.jsx
@@ -0,0 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import Home from "../pages/home.jsx"
+import About from "../pages/About.jsx"
+function AllRoutes() {
+  return (
+    <BrowserRouter>
+    <Routes>
+        <Route exact path="/" element={<Home/>} />
+        <Route exact path="/about" element={<About/>} />
+    </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default AllRoutes
